9c4e8a4b9391a32fb237b98b61164ee5e5fea722
David Goyette <davidg0yette@protonmail.com>
2025-02-10 17:29:52 -0500

    Delete cc_get.js

--- cc_get.js
+++
@@ -1,7 +0,0 @@
-if(window.location.href.includes('/approve')){
-    console.log('OrderMooose - ready to copy order total');
-    document.querySelector('input.btn.btn-secondary.btn-block').addEventListener('click',()=>{
-        const order_total=document.getElementById('esa_account_line_item_debit_total').value.replace(/[^0-9.]/g,'');
-        chrome.storage.local.set({order_total:order_total},()=>{console.log('OrderMoose - copied order total')});
-    });
-}
